723e702fb0abcfd966090d6eae24c58f510ba247
JulianaAmoasei <late.parrot@gmail.com>
2023-09-27 09:44:44 -0300

    video: validações no modelo

--- src/database/models/pessoa.js
+++ src/database/models/pessoa.js
@@ -17,7 +17,15 @@ module.exports = (sequelize, DataTypes) => {
   }
   Pessoa.init({
     nome: DataTypes.STRING,
-    email: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: 'formato do email inválido'
+        }
+      }
+    },
     cpf: DataTypes.STRING,
     ativo: DataTypes.BOOLEAN,
     role: DataTypes.STRING

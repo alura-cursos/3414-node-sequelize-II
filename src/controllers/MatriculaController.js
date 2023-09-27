94fb427d81819c735d0f0400a6ac9af2fecedc03
JulianaAmoasei <late.parrot@gmail.com>
2023-09-27 16:21:39 -0300

    video: agrupando com literais

--- src/controllers/MatriculaController.js
+++ src/controllers/MatriculaController.js
@@ -1,3 +1,5 @@
+const Sequelize = require('sequelize');
+
 const Controller = require('./Controller.js');
 const MatriculaServices = require('../services/MatriculaServices.js');
 
@@ -11,10 +13,16 @@ class MatriculaController extends Controller {
   async pegaMatriculasPorEstudante(req, res) {
     const { estudante_id } = req.params;
     try {
-      const listaMatriculasPorEstudante = await matriculaServices.pegaEContaRegistros({
-        estudante_id: Number(estudante_id),
-        status: 'matriculado'
-      });
+      const listaMatriculasPorEstudante = await matriculaServices.pegaEContaRegistros(
+        {
+          where: {
+            estudante_id: Number(estudante_id),
+            status: 'matriculado'
+          },
+          limit: 2,
+          order: [['id', 'DESC']]
+        }
+      );
       return res.status(200).json(listaMatriculasPorEstudante);
     } catch (erro) {
       return res.status(500).json({ erro: erro.message });
@@ -24,10 +32,16 @@ class MatriculaController extends Controller {
   async pegaCursosLotados (req, res) {
     const lotacaoCurso = 2;
     try {
-      const cursosLotados = await matriculaServices.pegaEContaRegistros({
-        status: 'matriculado'
-      });
-      return res.status(200).json(cursosLotados);
+      const cursosLotados = await matriculaServices.pegaEContaRegistros(
+        {
+          where: {
+            status: 'matriculado'
+          },
+          attributes: ['curso_id'],
+          group: ['curso_id'],
+          having: Sequelize.literal(`count(curso_id) >= ${lotacaoCurso}`)
+        });
+      return res.status(200).json(cursosLotados.count);
     } catch (erro) {
       return res.status(500).json({ erro: erro.message });
     }
--- src/services/Services.js
+++ src/services/Services.js
@@ -21,12 +21,8 @@ class Services {
     return dataSource[this.model].findOne({ where: { ...where } });
   }
 
-  async pegaEContaRegistros(where) {
-    return dataSource[this.model].findAndCountAll({
-      where: { ...where },
-      // limit: 2,
-      order: [['id', 'DESC']]
-    });
+  async pegaEContaRegistros(options) {
+    return dataSource[this.model].findAndCountAll({ ...options });
   }
 
   async criaRegistro(dadosDoRegistro) {

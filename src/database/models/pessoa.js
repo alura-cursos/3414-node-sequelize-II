2f81f86c8618089d698bc2de23d64147aeda575a
JulianaAmoasei <late.parrot@gmail.com>
2023-09-27 10:52:14 -0300

    video: associação estudante e matricula

--- src/database/models/pessoa.js
+++ src/database/models/pessoa.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
       });
       Pessoa.hasMany(models.Matricula, {
         foreignKey: 'estudante_id',
-        // scope: { status: 'matriculado' },
+        scope: { status: 'matriculado' },
         as: 'aulasMatriculadas'
       });
     }
